90c1896e59bb11a9012e2657d76e0fdae862ef48
zhangtony239 <zhangtony239@gmail.com>
2026-05-24 05:42:40 +0800

    accelerate fp32 with tf32

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import torch
 import numpy as np
 from diffusers.pipelines.stable_diffusion_xl.pipeline_stable_diffusion_xl import StableDiffusionXLPipeline
 from diffusers.schedulers.scheduling_euler_ancestral_discrete import EulerAncestralDiscreteScheduler
+#from diffusers.schedulers.scheduling_dpmsolver_multistep import DPMSolverMultistepScheduler
 from compel import CompelForSDXL
 from random import randint
 
 # --- 配置区 ---
-#ollama_path = "C:\\Users\\Tony\\应用\\ollama-ipex-llm\\"
 ckpt_path = "miaomiaoRealskin_vPredV11.safetensors"
 output_path = "C:\\Users\\Tony\\Downloads\\"
 negative_prompt = "worst quality,bad quality,simple_background,low quality,jpeg artifacts,old,oldest,signature,shiny_skin,bad hands,bad feet,"
@@ -15,21 +15,13 @@ hotwords = {
     }
 # --- 配置区 ---
 
-'''Ollama 暂不常驻
-ollama_info = subprocess.run(ollama_path+"ollama ps", text=True, capture_output=True).stdout
-if len(ollama_info.splitlines()) > 1:
-    print("Detected Ollama's model alive, stopping...")
-    for info in ollama_info.splitlines()[1:]:
-        name = info.split(' ')[0]
-        subprocess.run(ollama_path+"ollama stop "+name)
-        print('Stopped '+name)
-'''
 def vae_forward_wrapper(original_forward):
     def wrapper(sample, *args, **kwargs):
         # 强制将输入转为 float32
         return original_forward(sample.to(dtype=torch.float32), *args, **kwargs)
     return wrapper
 
+torch.set_float32_matmul_precision("high")
 pipe = StableDiffusionXLPipeline.from_single_file(
     ckpt_path,
     use_safetensors=True,
@@ -38,6 +30,7 @@ pipe = StableDiffusionXLPipeline.from_single_file(
 )
 scheduler_args = {"prediction_type": "v_prediction", "rescale_betas_zero_snr": True}
 pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(pipe.scheduler.config, **scheduler_args)
+#pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config, **scheduler_args)
 pipe.text_encoder.config.num_hidden_layers -= 2 # CLIP skip: 2
 pipe.vae.decode = vae_forward_wrapper(pipe.vae.decode)
 pipe.vae.enable_tiling() # 解锁更高分辨率
@@ -66,7 +59,7 @@ def draw(prompt,seed):
         height=1536,
         num_inference_steps=30,
         guidance_scale=5,
-        generator=torch.Generator().manual_seed(seed),
+        generator=torch.Generator(device="cpu").manual_seed(seed),
     ).images[0] # pyright: ignore[reportAttributeAccessIssue]
     
     image.save(f"{output_path}{seed}.png")
