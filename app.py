bc5ec856381645e58b91e676c087e847db4d708b
zhangtony239 <zhangtony239@gmail.com>
2026-06-11 20:14:07 +0800

    add common dataset

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from diffusers.pipelines.stable_diffusion_xl.pipeline_stable_diffusion_xl import
 from diffusers.schedulers.scheduling_euler_ancestral_discrete import EulerAncestralDiscreteScheduler
 from compel import CompelForSDXL
 from random import randint
-from prompt_toolkit import prompt as pt_prompt
+from prompt_toolkit import prompt
 from prompt_toolkit.completion import Completer, Completion
 import duckdb
 
@@ -115,12 +115,14 @@ if __name__ == "__main__":
     try:
         db = duckdb.connect()
         query = """
-            SELECT character FROM (
-                SELECT character, count FROM 'tags/danbooru_character.csv' WHERE count >= 100
+            SELECT tag FROM (
+                SELECT character AS tag, count FROM read_csv('tags/danbooru_character.csv', ignore_errors=true) WHERE count >= 100
                 UNION ALL
-                SELECT character, count FROM 'tags/e621_character.csv' WHERE count >= 100
+                SELECT character AS tag, count FROM read_csv('tags/e621_character.csv', ignore_errors=true) WHERE count >= 100
+                UNION ALL
+                SELECT tags AS tag, count FROM read_csv('tags/danbooru_e621_merged.csv', ignore_errors=true) WHERE count >= 100
             )
-            GROUP BY character
+            GROUP BY tag
             ORDER BY MAX(count) DESC
         """
         results = db.execute(query).fetchall()
@@ -138,18 +140,18 @@ if __name__ == "__main__":
     
     while True:
         try:
-            prompt = pt_prompt("Prompt: ", completer=completer).strip()
+            prompts = prompt("Prompt: ", completer=completer).strip()
         except KeyboardInterrupt:
             print()
             continue
-        if prompt in ['Q','q','exit']:
+        if prompts in ['Q','q','exit']:
             break
-        elif len(prompt.split('seed')) > 1:
-            seed = int(prompt.split('seed')[1].split(',')[0][1:])
-            prompt = prompt.split('seed')[0] + ','.join(prompt.split('seed')[1].split(',')[1:])
+        elif len(prompts.split('seed')) > 1:
+            seed = int(prompts.split('seed')[1].split(',')[0][1:])
+            prompts = prompts.split('seed')[0] + ','.join(prompts.split('seed')[1].split(',')[1:])
         else:
             seed = randint(0, MAX_SEED)
-        prompt_tags = [t.strip() for t in prompt.split(',')]
+        prompt_tags = [t.strip() for t in prompts.split(',')]
         processed_tags = [hotwords[t.lower()] if t.lower() in hotwords else t for t in prompt_tags]
-        prompt = ",".join(processed_tags)
-        draw(prompt, seed)
+        prompts = ",".join(processed_tags)
+        draw(prompts, seed)
